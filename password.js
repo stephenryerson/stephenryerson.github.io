f226210cee0e4c73db39f656bee30ae4a3f15d33
stephenryerson <iStephenN@hotmail.com>
2016-10-01 21:07:20 -0400

    Delete password.js
    stephenryerson

--- password.js
+++
@@ -1,6 +0,0 @@
-var password = "test12345";
-var input = prompt('Enter the password',' ');
-while (password != input)
-{
-  var input = prompt('Access Denied: Invalid Password. Please try again',' ');
-}
