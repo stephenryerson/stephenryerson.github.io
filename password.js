5d4dfb6b3959f9dd9cc2c94f927fbb84d648ff1c
stephenryerson <iStephenN@hotmail.com>
2016-10-01 21:02:15 -0400

    Create password.js

---
+++ password.js
@@ -0,0 +1,6 @@
+var password = "test12345";
+var input = prompt('Enter the password',' ');
+while (password != input)
+{
+  var input = prompt('Access Denied: Invalid Password. Please try again',' ');
+}
