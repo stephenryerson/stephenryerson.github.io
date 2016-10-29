47cd3c80fd391efbef347615371d23884910f668
stephenryerson <iStephenN@hotmail.com>
2016-10-28 22:57:22 -0400

    Update lab4p1.pl

--- cgi-bin/lab4p1.pl
+++ cgi-bin/lab4p1.pl
@@ -3,7 +3,7 @@
 use CGI':standard';
 use strict;
 
-print "Content type: text/html\n\n";
+print "Content-type: text/html\n\n";
 
 print '<link rel="stylesheet" type="text/css" href="lab4.css" charset="utf-8">';
 print '<h1>This is my first Perl program!</h1>';
