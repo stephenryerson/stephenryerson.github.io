9ad09029707b9644e5810215d84eb9d8b7c531ec
stephenryerson <iStephenN@hotmail.com>
2016-10-28 21:08:32 -0400

    Create lab4p1.pl

---
+++ cgi-bin/lab4p1.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl
+
+use CGI':standard';
+use strict;
+
+print "Content type: text/html\n\n";
+
+print '<link rel="stylesheet" type="text/css" href="lab4.css" charset="utf-8">';
+print '<h1>This is my first Perl program!</h1>';
